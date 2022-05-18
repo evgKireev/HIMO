21cffefd9572b26ef63cc69a9970a64361d63b32
evgKireev <bob1_k@mail.ru>
2022-05-19 02:15:37 +0300

    him

---
+++ app/js/main.js
@@ -0,0 +1,13 @@
+$(function() {
+  $('.header-content__bike').slick({
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows: false,
+  });
+
+  $('.gallery__wrapper').slick({
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows: false,
+  });
+});
